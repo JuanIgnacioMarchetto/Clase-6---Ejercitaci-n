cf1b8c76b0cbe0ce52cbb96cf13e1f709032eea8
Juan Ignacio Marchetto <juanignaciomarchetto@gmail.com>
2023-11-22 08:44:44 -0300

    22/11/2023_08:44

--- src/components/Form.js
+++ src/components/Form.js
@@ -23,69 +23,72 @@ class Form extends React.Component {
         return (
             <div className='container'>
                 <h2>Registro</h2>
-                <form className='grid-container'>
-                    <label className='grid-item'>
-                        Nombre:
-                        <input
-                            type="text"
-                            name="nombre"
-                            value={this.state.nombre}
-                            onChange={this.handleChange}
-                        />
-                    </label>
+                <div>
 
-                    <label className='grid-item'>
-                        Apellido:
-                        <input
-                            type="text"
-                            name="apellido"
-                            value={this.state.apellido}
-                            onChange={this.handleChange}
-                        />
-                    </label>
+                    <form className='grid-container'>
+                        <label className='grid-item'>
+                            Nombre:
+                            <input
+                                type="text"
+                                name="nombre"
+                                value={this.state.nombre}
+                                onChange={this.handleChange}
+                            />
+                        </label>
 
-                    <label className='grid-item'>
-                        Direccion de Email:
-                        <input
-                            type="email"
-                            name="email"
-                            value={this.state.email}
-                            onChange={this.handleChange}
-                        />
-                    </label>
+                        <label className='grid-item'>
+                            Apellido:
+                            <input
+                                type="text"
+                                name="apellido"
+                                value={this.state.apellido}
+                                onChange={this.handleChange}
+                            />
+                        </label>
 
-                    <label className='grid-item'>
-                        Teléfono:
-                        <input
-                            type="text"
-                            name="telefono"
-                            value={this.state.telefono}
-                            onChange={this.handleChange}
-                        />
-                    </label>
+                        <label className='grid-item'>
+                            Direccion de Email:
+                            <input
+                                type="email"
+                                name="email"
+                                value={this.state.email}
+                                onChange={this.handleChange}
+                            />
+                        </label>
 
-                    <label className='grid-item'>
-                        Contraseña:
-                        <input
-                            type="password"
-                            name="password"
-                            value={this.state.password}
-                            onChange={this.handleChange}
-                        />
-                    </label>
+                        <label className='grid-item'>
+                            Teléfono:
+                            <input
+                                type="text"
+                                name="telefono"
+                                value={this.state.telefono}
+                                onChange={this.handleChange}
+                            />
+                        </label>
 
-                    <label className='grid-item'>
-                        Confirmar Contraseña:
-                        <input
-                            type="password"
-                            name="confirmarPassword"
-                            value={this.state.confirmarPassword}
-                            onChange={this.handleChange}
-                        />
-                    </label>
+                        <label className='grid-item'>
+                            Contraseña:
+                            <input
+                                type="password"
+                                name="password"
+                                value={this.state.password}
+                                onChange={this.handleChange}
+                            />
+                        </label>
 
-                    <button type="submit">Registrar</button>
-                </form>
+                        <label className='grid-item'>
+                            Confirmar Contraseña:
+                            <input
+                                type="password"
+                                name="confirmarPassword"
+                                value={this.state.confirmarPassword}
+                                onChange={this.handleChange}
+                            />
+                        </label>
+
+                        <button type="submit">Registrar</button>
+                    </form>
+                </div>
             </div>
         );
     }
