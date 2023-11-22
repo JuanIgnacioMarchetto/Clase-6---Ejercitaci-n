e7161a9d1c5569597f48fcbffbb6c37d29862210
Juan Ignacio Marchetto <juanignaciomarchetto@gmail.com>
2023-11-22 07:10:27 -0300

    22/11/2023_07:10

--- src/App.js
+++ src/App.js
@@ -1,99 +1,10 @@
 import React from 'react';
+import Form from './components/Form'
+function App()
+{
+  <div>
 
-class RegistroPagina extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      nombre: '',
-      apellido: '',
-      email: '',
-      telefono: '',
-      password: '',
-      confirmarPassword: ''
-    };
-  }
+<Form></Form>
 
-  handleChange = (event) => {
-    const { name, value } = event.target;
-    this.setState({ [name]: value });
-  }
-
-  render() {
-    return (
-      <div>
-        <h2>Registro</h2>
-        <form>
-          <label>
-            Nombre:
-            <input
-              type="text"
-              name="nombre"
-              value={this.state.nombre}
-              onChange={this.handleChange}
-            />
-          </label>
-          <br />
-
-          <label>
-            Apellido:
-            <input
-              type="text"
-              name="apellido"
-              value={this.state.apellido}
-              onChange={this.handleChange}
-            />
-          </label>
-          <br />
-
-          <label>
-            Email:
-            <input
-              type="email"
-              name="email"
-              value={this.state.email}
-              onChange={this.handleChange}
-            />
-          </label>
-          <br />
-
-          <label>
-            Teléfono:
-            <input
-              type="text"
-              name="telefono"
-              value={this.state.telefono}
-              onChange={this.handleChange}
-            />
-          </label>
-          <br />
-
-          <label>
-            Password:
-            <input
-              type="password"
-              name="password"
-              value={this.state.password}
-              onChange={this.handleChange}
-            />
-          </label>
-          <br />
-
-          <label>
-            Confirmar Password:
-            <input
-              type="password"
-              name="confirmarPassword"
-              value={this.state.confirmarPassword}
-              onChange={this.handleChange}
-            />
-          </label>
-          <br />
-
-          <button type="submit">Registrar</button>
-        </form>
-      </div>
-    );
-  }
+  </div>
 }
-
-export default RegistroPagina;
